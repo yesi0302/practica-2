6124d9f53030cae16230c453586922cca6ef59d7
yesi0302 <yesi.islas.rive.m.22@gmail.com>
2023-04-13 22:14:58 -0600

    first commit

---
+++ practica/app/src/main/java/com/example/bnv/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.bnv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.bnv.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private NavHostFragment navHostFragment;
+
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
+
+    }
+
+    private void initNavigation() {
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
+    }
+}
